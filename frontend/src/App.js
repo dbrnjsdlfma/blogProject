f5ffb6038266bdf1116668fb8f4b9cf1f9d68c97
dbrnjsdlfma <dbrnjsdlfma@naver.com>
2023-11-28 18:11:46 +0900

    포트폴리오 웹 사이트 구도 및 css 작성중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,66 @@ function App() {
   return (
     <div className="App">
         <header>
-        <div className="logo">
-            <a href="#"><h1>YG Info Blog</h1></a>
-        </div>
-        <nav>
-            <ul>
-                <li><button>버튼 1</button></li>
-                <li><button>버튼 2</button></li>
-                <li><button>버튼 3</button></li>
-            </ul>
-        </nav>
+            <nav>
+                <div className="logo">
+                    <a href="#"><h1>YuGwon Portfolio</h1></a>
+                </div>
+                <ul>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </nav>
         </header>
         <main>
-            lorem
+            <section>
+                <div className="container">
+                    <div className="content">
+                        <div className="title">
+                            <h1>안녕하세요<br/>신입 프론트엔드 개발자 입니다.</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="aboutme">
+                    <div className="section_title">
+                        <h2>About me</h2>
+                    </div>
+                    <div className="aboutme_body">
+                        <article>
+                            <span>이름</span>
+                            <p>유권</p>
+                        </article>
+                        <article>
+                            <span>생년월일</span>
+                            <p>1993. 07. 28</p>
+                        </article>
+                        <article>
+                            <span>연락처</span>
+                            <p>010 - 9517 - 4790</p>
+                            <p>dbrnjsdlfma@gmail.com</p>
+                        </article>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="skills">
+                    <div className="section_title">
+                        <h2>About me</h2>
+                    </div>
+                </div>
+            </section>
         </main>
         <footer>
-            <div className="">
-
-            </div>
+            <address>
+                <span>Copyright © 2023 YuGwon Portfolio. All rights reserved.</span>
+            </address>
+            <button>
+                <span class="material-symbols-outlined">
+                    arrow_upward
+                </span>
+            </button>
         </footer>
     </div>
   );
